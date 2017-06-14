5c54ba78fcd9daadf7903f0cca67fa5e394af8ab
mizne <w20054319@126.com>
2017-06-14 09:50:55 +0800

    update ErrorMessage model

--- app/routes/error-message.js
+++ app/routes/error-message.js
@@ -5,4 +5,12 @@ router.get('/api/v1/error-message', ErrorMessage.find)
 
 router.post('/api/v1/error-message', ErrorMessage.save)
 
+router.get('/api/v1/ip', async (ctx, next) => {
+  console.log(ctx.request.ip)
+
+  ctx.body = {
+    ip: ctx.request.ip
+  }
+})
+
 module.exports = router
