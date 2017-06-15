005cb122be53ab3ab5b7716648285317c0131b31
mizne <w20054319@126.com>
2017-06-15 14:27:46 +0800

    update

--- app/routes/error-message.js
+++ app/routes/error-message.js
@@ -5,15 +5,4 @@ router.get('/api/v1/error-message', ErrorMessage.find)
 
 router.post('/api/v1/error-message', ErrorMessage.save)
 
-router.get('/api/v1/ip', async (ctx, next) => {
-	const req = ctx.request
-
-
-  ctx.body = {
-    forward: req.header['x-forwarded-for'],
-    remoteAddress: ctx.req.connection.RemoteAddress,
-    realIp: req.header['x-real-ip']
-  }
-})
-
 module.exports = router
